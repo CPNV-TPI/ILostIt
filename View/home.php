d73c1ad2fed5cd7679ffb94dec9ea7e51a76aa42
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-05-03 14:43:41 +0200

    feat: Add last posts slider and page text followin the design

--- View/home.php
+++ View/home.php
@@ -1,16 +1,35 @@
 <?php
 namespace ILostIt\View;
-
-ob_start();
-
-$title = "Home";
 ?>
 
 <div class="home">
-    <p>Home page</p>
-</div>
+    <div class="content">
+        <div class="last-posts space-y-5 md:space-y-10 text-center">
+            <div class="title text-4xl pt-5 md:pt-10">Bienvenue sur la plateforme !</div>
+            <div class="sep w-1/3 h-px bg-black mx-auto"></div>
+            <div class="desc">Les dernières publications :</div>
 
-<?php
-$pageContent = ob_get_clean();
-require 'gabarit.php';
-?>
+            <div class="slider">
+                <div class="splide slider-last-posts md:w-7/9 mx-auto">
+                    <div class="splide__track">
+                        <ul class="splide__list">
+                            <li class="splide__slide">
+                                <div class="splide__slide__container w-2/3 mx-auto py-2 h-96 bg-green-700"></div>
+                            </li>
+                            <li class="splide__slide">
+                                <div class="splide__slide__container w-2/3 mx-auto py-2 h-96 bg-blue-700"></div>
+                            </li>
+                            <li class="splide__slide">
+                                <div class="splide__slide__container w-2/3 mx-auto py-2 h-96 bg-red-700"></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <?php if(!isset($_SESSION)): ?>
+                <a href="/posts" class="bg-primary px-20 py-5 text-white">Voir plus</a>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
