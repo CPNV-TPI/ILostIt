92a463724a25a3c08163a06f3ebe410494b34f4c
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-05-06 12:02:40 +0200

    feat: Add controller to handle Mod requests

---
+++ Controller/ModController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ILostIt\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use ILostIt\Model\PostsModel;
+use Slim\Views\PhpRenderer;
+
+class ModController
+{
+    public function get(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $render = new PhpRenderer(__DIR__ . '/../View', ['title' => 'Modération']);
+        $render->setLayout('gabarit.php');
+
+        return $render->render($response, 'mod.php', ["posts" => []]);
+    }
+}
