07685858a7b6e63ac8b5bfdd97d18ecc7012051f
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-05-06 12:03:02 +0200

    feat: Add Post page

--- View/components/post.php
+++ View/components/post.php
@@ -1,3 +1,18 @@
-<div class="post w-96 border-2 rounded-md p-4">
-
-</div>
+<a href="posts/<?=$postId?>" class="w-full md:w-[350px] lg:w-[500px]">
+    <div class="post border-2 rounded-md p-4">
+        <div class="content flex space-x-2">
+            <div class="left flex flex-col w-3/12 items-center">
+                <img src="src/img/post_base_image.png" class="w-[50px] lg:w-[70px]">
+                <p class="text-sm"><?=$classroomNumber?></p>
+            </div>
+            <div class="right w-9/12">
+                <div class="title text-center text-lg font-bold max-w-[20ch] text-ellipsis overflow-hidden whitespace-nowrap">
+                    <?=$title?>
+                </div>
+                <div class="description text-ellipsis max-w-[100ch] line-clamp-2 lg:line-clamp-3">
+                    <?=$description?>
+                </div>
+            </div>
+        </div>
+    </div
+</a>
