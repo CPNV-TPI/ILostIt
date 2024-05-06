a2458b81028968324e17187225e4fe19c256904d
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-05-06 11:57:45 +0200

    refactor: Move from app.js Splide's sliders
    Move from app.js to View/home.php and View/post.php Splide's sliders configs to prevent errors if it not exits on other pages.

--- src/js/app.js
+++ src/js/app.js
@@ -10,38 +10,12 @@ const nav_hamburger = document.querySelector('.hamburger')
 const nav_menu = document.querySelector('.links')
 
 nav_hamburger.addEventListener('click', () => {
-    if (nav_menu.classList.contains('hidden'))
+    if (nav_menu.classList.contains('hidden')) {
         nav_menu.classList.replace('hidden', 'flex')
-    else
+    } else {
         nav_menu.classList.replace('flex', 'hidden')
+    }
 })
-
-/* Last posts slider */
-var splide = new Splide( '.splide', {
-    type: 'loop',
-    pagination: false
-});
-
-function changeNbOfPostsBasedOnWidth() {
-    if(window.innerWidth > 425)
-        splide.options.perPage = 2
-
-    if(window.innerWidth > 768)
-        splide.options.perPage = 3
-
-    if(window.innerWidth <= 425)
-        splide.options.perPage = 1
-}
-
-document.addEventListener( 'DOMContentLoaded', function() {
-    changeNbOfPostsBasedOnWidth()
-} );
-
-window.onresize = () => {
-    changeNbOfPostsBasedOnWidth()
-}
-
-splide.mount();
 
 /* Post menu */
 const post_btn = document.getElementById('post_btn')
