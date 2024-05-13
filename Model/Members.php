9fe2b473a142f84959f4a5ce62875023bff0a88a
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-05-13 15:32:50 +0200

    feat: Add method to verify members and update methods comments

--- Model/Members.php
+++ Model/Members.php
@@ -4,19 +4,32 @@ namespace ILostIt\Model;
 
 class Members
 {
+    /**
+     * This method is designed to get the members
+     * @param array $filters Example -> array(
+     *   ["column1", "sql_operator", "%value1%"],
+     *   ["column1", "sql_operator", "%value1%"]
+     *  ) /!\ FILTERS ARE EXCLUSIVELY ANDs !
+     * @return array
+     */
     public function getMembers(array $filters = []): array
     {
         $db = new Database();
 
         $table = "members";
-        $columns = ["firstname", "lastname", "email", "password", "isVerified", "isActive", "isMod"];
+        $columns = ["id", "firstname", "lastname", "email", "password", "isVerified", "isActive", "isMod"];
 
         return $db->select($table, $columns, $filters);
     }
 
-    public function registerNewMember(array $values): bool | string
+    /**
+     * This method is designed to handle the register of a new member
+     * @param array $memberInformations Example -> array("column1" => "value1", "column2" => "value2")
+     * @return bool|string
+     */
+    public function registerNewMember(array $memberInformations): bool | string
     {
-        $filters = [ ["email", "=", $values["email"]] ];
+        $filters = [ ["email", "=", $memberInformations["email"]] ];
 
         $user = $this->getMembers($filters);
 
@@ -24,15 +37,17 @@ class Members
             return "L'email est déjà utilisé !";
         }
 
-        $values["password"] = hash('sha256', $values["password"]);
+        $memberInformations["password"] = hash('sha256', $memberInformations["password"]);
 
         $db = new Database();
-        $response = $db->insert("members", $values);
+        $response = $db->insert("members", $memberInformations);
         $db = null;
 
         if (!$response) {
             return "Une erreur s'est produite";
         }
+
+        $user = $this->getMembers($filters)[0];
 
         $mail = new Emails();
 
@@ -40,8 +55,27 @@ class Members
         $message = "Bonjour!<br/><br/>Tout d'abord, bienvenue !<br/><br/>";
         $message .= "Pour accéder à notre site, vous devez tout d'abord vérifier votre email !<br/><br/>";
         $message .= "Pour cela, veuillez appuier sur le lien suivant : ";
-        $message .= "http://localhost:8080/auth/register/validate?email=" . $values["email"];
+        $message .= "http://localhost:8080/auth/register/verify/" . $user["id"];
 
-        return $mail->send($values["email"], $message, $subject);
+        return $mail->send($memberInformations["email"], $message, $subject);
+    }
+
+    /**
+     * This method is designed to verify a newly created user
+     * @param string $id
+     * @return bool
+     */
+    public function verifyUser(string $id): bool
+    {
+        $db = new Database();
+
+        $table = "members";
+        $response = $db->update($table, ["isVerified" => 1], [["id", "=", $id]]);
+
+        if (!$response) {
+            return false;
+        }
+
+        return true;
     }
 }
