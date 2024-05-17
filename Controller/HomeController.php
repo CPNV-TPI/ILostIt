821a929486801e66ffd08ada4e2eb5ba1977cad5
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-05-17 14:11:17 +0200

    feat: Add get of the last objects to show on home page

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -1,17 +1,32 @@
 <?php
 namespace ILostIt\Controller;
 
+use ILostIt\Model\Objects;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\PhpRenderer;
 
 class HomeController
 {
-    public function index(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
-    {
+    public function index(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args,
+        string $error = ""
+    ): ResponseInterface {
+        $objectsModel = new Objects();
+        $nbObjects = 6;
+        $filters = [["status", "=", 1]];
+        $orderBy = [["id", "DESC"]];
+        $objects = $objectsModel->getObjects($filters, 1, $nbObjects, $orderBy);
+
         $render = new PhpRenderer(__DIR__ . '/../View', ['title' => 'Accueil']);
         $render->setLayout('gabarit.php');
 
-        return $render->render($response, 'home.php');
+        if ($error != "") {
+            return $render->render($response, 'home.php', ['error' => $error, 'objects' => $objects]);
+        }
+
+        return $render->render($response, 'home.php', ['objects' => $objects]);
     }
 }
