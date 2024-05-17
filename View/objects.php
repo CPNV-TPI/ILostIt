b862b8a41f23068ea905c282a54e9d134c2a5e80
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-05-17 14:16:20 +0200

    feat: Update images get method and add previous/next page feature

--- View/objects.php
+++ View/objects.php
@@ -4,12 +4,12 @@ namespace ILostIt\View;
 
 ?>
 
-<div class="posts">
+<div class="posts py-5 md:py-10">
     <div class="content">
-        <div class="title text-4xl pt-5 md:pt-10 text-center">Les publications</div>
+        <div class="title text-4xl text-center">Les publications</div>
         <div class="filters text-center pt-5 md:pt-10">
-            <a href="/posts?type=Perdu" class="px-2 py-px rounded-lg bg-orange-300">Perdu</a>
-            <a href="/posts?type=Retrouvé" class="px-2 py-px rounded-lg bg-orange-300">Retrouvé</a>
+            <a href="/objects?type=Perdu" class="px-2 py-px rounded-lg bg-orange-300">Perdu</a>
+            <a href="/objects?type=Retrouvé" class="px-2 py-px rounded-lg bg-orange-300">Retrouvé</a>
         </div>
         <div class="
             posts w-5/6 mx-auto flex flex-wrap justify-between gap-y-5 items-center mt-10
@@ -20,7 +20,7 @@ namespace ILostIt\View;
             <?php else : ?>
                 <?php foreach ($objects as $object) :
                         $objectId = $object['id'];
-                        $image = json_decode($object['image'])[0];
+                        $image = isset($object['images']) ? $object['images'][0] : null;
                         $classroom = $object['classroom'];
                         $title = $object['title'];
                         $description = $object['description'];
@@ -31,17 +31,17 @@ namespace ILostIt\View;
         </div>
 
         <div class="previous-next-page text-center mt-10 space-x-2 md:space-x-5">
-            <?php if (isset($nbPreviousPage)) : ?>
+            <?php if (isset($previousPage)) : ?>
                 <a
                     class="bg-primary py-4 px-5 md:px-10 text-white"
-                    href="/posts?page=<?=$nbPreviousPage?>"
+                    href="/objects?<?=isset($byType) ? "type=" . $byType . "&" : ""?>page=<?=$previousPage?>"
                 >Page précédente</a>
             <?php endif; ?>
 
-            <?php if (isset($nbNextPage)) : ?>
+            <?php if (isset($nextPage)) : ?>
                 <a
                     class="bg-primary py-4 px-5 md:px-10 text-white"
-                    href="/posts?page=<?=$nbNextPage?>"
+                    href="/objects?<?=isset($byType) ? "type=" . $byType . "&" : ""?>page=<?=$nextPage?>"
                 >Page suivante</a>
             <?php endif; ?>
         </div>
